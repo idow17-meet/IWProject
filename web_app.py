96480f789b6d52a24ca5734851fc33ae45b0b6c9
Ido <idowiernik2000@gmail.com>
2017-02-12 18:33:39 +0200

    oh oh

--- web_app.py
+++ web_app.py
@@ -18,6 +18,7 @@ import hashlib
 #SECRET
 #Client id: 606083212870-invusvesj5a2khknk813hrupq27h35k6.apps.googleusercontent.com 
 #client secret:  0Wbve1W1lOrRvoUquT4SFq2k 
+#g+ key: AIzaSyAqSCkJphmlRwqDqOMGD9PT7Ld6mF5R5ZI
 
 #My google id:
 #103891305576825217486
@@ -35,8 +36,8 @@ google = oauth.remote_app('google',
 					  base_url='https://www.google.com/accounts/',
 					  authorize_url='https://accounts.google.com/o/oauth2/auth',
 					  request_token_url=None,
-					  request_token_params={'scope': 'https://www.googleapis.com/auth/userinfo.email',#' '.join(scope),
-											'response_type': 'code'},
+					  request_token_params={'scope': 'https://www.googleapis.com/auth/userinfo.email',
+ 											'response_type': 'code'},
 					  access_token_url='https://accounts.google.com/o/oauth2/token',
 					  access_token_method='POST',
 					  access_token_params={'grant_type': 'authorization_code'},
